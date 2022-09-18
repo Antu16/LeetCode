f14ebffaa098340762ce8d7caf76dc4dfc86e201
Antu16 <mahfujulalam81@gmail.com>
2022-09-18 14:30:54 +0600

    Time: 11 ms (34.37%), Space: 43.1 MB (87.39%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,8 +1,27 @@
+import java.util.Map.Entry;
 class Solution {
     public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> numbers = new HashMap<>();
         int res = 0;
-        for (int num : nums){
-            res ^= num;
+        for (int i = 0; i< nums.length; i++){
+            if (numbers.containsKey(nums[i])){
+                int val = numbers.get(nums[i]);
+                val++;
+                numbers.put(nums[i], val);
+            
+            }
+            else{
+                numbers.put(nums[i], 1);
+            }
+        }
+        
+        for(Entry<Integer, Integer> entry: numbers.entrySet()) {
+
+          if(entry.getValue() == 1) {
+            
+              res = entry.getKey();
+            
+          }
         }
         return res;
     }
